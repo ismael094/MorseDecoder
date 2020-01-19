e238e1b7aaf63a84a69a7378a4dcccb27e578284
ismael094 <ismaelpitufo@gmail.com>
2020-01-19 09:55:16 +0000

    Second test: red test

--- src/test/java/MorseDecoder.java
+++ src/test/java/MorseDecoder.java
@@ -4,8 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class MorseDecoder {
     @Test
-    public void order_an_empty_string_should_return_empty() {
+    public void decode_an_empty_string_should_return_empty() {
         assertThat(decode("")).isEqualTo("");
+    }
+
+    @Test
+    public void decode_an_point_string_should_return_letter_E() {
+        assertThat(decode(".")).isEqualTo("E");
     }
 
     private String decode(String phrase) {
